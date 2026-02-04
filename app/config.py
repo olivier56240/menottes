fab40e631f709b017b3fc80c2eddf0f431220488
Olivier Foin <dooky56240@gmail.com>
2026-02-04 14:52:56 +0100

    fix sqlalchemy database uri config

--- app/config.py
+++ app/config.py
@@ -2,14 +2,11 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-class DevConfig:
-   SECRET_KEY = "plouay"
-
 db_url = os.environ.get("DATABASE_URL")
+if db_url and db_url.startswith("postgres://"):
+   db_url = db_url.replace("postgres://", "postgresql://", 1)
 
-if db_url:
-   if db_url.startswith("postgres://"):
-       db_url = db_url.replace("postgres://", "postgresql://", 1)
-   SQLALCHEMY_DATABASE_URI = db_url
-else:
-   SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "instance/local.db")
+class DevConfig:
+   SECRET_KEY = os.environ.get("SECRET_KEY", "plouay")
+   SQLALCHEMY_DATABASE_URI = db_url or "sqlite:///" + os.path.join(BASE_DIR, "instance/local.db")
+   SQLALCHEMY_TRACK_MODIFICATIONS = False

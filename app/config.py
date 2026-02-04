a292c7d8d85947de8d8c9e16751c2fee652152f8
Olivier Foin <dooky56240@gmail.com>
2026-02-04 14:07:08 +0100

    Add postgres driver

--- app/config.py
+++ app/config.py
@@ -5,5 +5,11 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 class DevConfig:
    SECRET_KEY = "plouay"
 
-   SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "..", "instance", "app.db")
-   SQLALCHEMY_TRACK_MODIFICATIONS = False
+db_url = os.environ.get("DATABASE_URL")
+
+if db_url:
+   if db_url.startswith("postgres://"):
+       db_url = db_url.replace("postgres://", "postgresql://", 1)
+   SQLALCHEMY_DATABASE_URI = db_url
+else:
+   SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "instance/local.db")

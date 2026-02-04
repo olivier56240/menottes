11926fae74ab8957f97a021ff230adde30b93980
Olivier Foin <dooky56240@gmail.com>
2026-02-04 15:35:05 +0100

    fix config for render postgres

--- app/config.py
+++ app/config.py
@@ -2,11 +2,16 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-db_url = os.environ.get("DATABASE_URL")
-if db_url and db_url.startswith("postgres://"):
-   db_url = db_url.replace("postgres://", "postgresql://", 1)
-
 class DevConfig:
-   SECRET_KEY = os.environ.get("SECRET_KEY", "plouay")
-   SQLALCHEMY_DATABASE_URI = db_url or "sqlite:///" + os.path.join(BASE_DIR, "instance/local.db")
+   SECRET_KEY = "dev"
+
+   db_url = os.environ.get("DATABASE_URL")
+
+   if db_url:
+       if db_url.startswith("postgres://"):
+           db_url = db_url.replace("postgres://", "postgresql://", 1)
+       SQLALCHEMY_DATABASE_URI = db_url
+   else:
+       SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "instance/local.db")
+
    SQLALCHEMY_TRACK_MODIFICATIONS = False

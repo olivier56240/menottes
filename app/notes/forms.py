cb3edff5cca5f273658d943cc826504b719c9a87
Olivier Foin <dooky56240@gmail.com>
2026-02-08 12:11:31 +0100

    Fix forms.py: clean categories and fields

--- app/notes/forms.py
+++ app/notes/forms.py
@@ -1,39 +1,49 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SelectField, SubmitField
-from wtforms.validators import DataRequired, Length
-from wtforms.fields import DateTimeLocalField
-from wtforms.validators import Optional
+from wtforms import (
+   StringField,
+   TextAreaField,
+   SelectField,
+   DateTimeLocalField
+)
+from wtforms.validators import DataRequired, Optional, Length
+
+# Catégories disponibles dans toute l'app
+CATEGORY_CHOICES = [
+   ("voiture", "Voiture"),
+   ("moto", "Moto"),
+   ("enduro", "Enduro"),
+   ("campingcar", "Camping-car"),
+   ("4x4", "4x4"),
+   ("bourse", "Bourse"),
+   ("ballade", "Balade"),
+]
 
 class NoteForm(FlaskForm):
    title = StringField(
        "Nom du rassemblement",
-       validators=[DataRequired(), Length(min=2, max=80)],
+       validators=[DataRequired(), Length(min=2, max=80)]
    )
 
    category = SelectField(
        "Catégorie",
-       choices=[
-           ("voiture", "Voiture"),
-           ("moto", "Moto"),
-           ("balade", "Balade"),
-       ],
-       validators=[DataRequired()],
+       choices=CATEGORY_CHOICES,
+       validators=[DataRequired()]
    )
 
    content = TextAreaField(
        "Description",
-       validators=[DataRequired(), Length(min=2, max=2000)],
+       validators=[Optional()]
    )
 
    location = StringField(
        "Lieu",
-       validators=[Optional()],
+       validators=[Optional()]
    )
 
    start_at = DateTimeLocalField(
        "Date et heure",
        format="%Y-%m-%dT%H:%M",
-       validators=[Optional()],
+       validators=[Optional()]
    )
 
-   submit = SubmitField("Enregistrer")
+   
